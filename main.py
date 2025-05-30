46909c08691451293e75b87ffa53a332010a9242
Angelyo29 <angeloseguera29b@gmail.com>
2025-05-30 15:20:28 -0600

    Se agregó el seno
    Signed-off-by: Angelyo29 <angeloseguera29b@gmail.com>

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ def generate_data():
     y = x ** 2
     return x, y
 
+def generate_data2():
+    """Generates x values and their squared values"""
+    x = np.linspace(-10, 10, 100)
+    y = x ** 3
+    return x, y
+
+def generate_data3():
+    """Generates x values and their squared values"""
+    x = np.linspace(-10, 10, 100)
+    y = np.sin(x)
+    return x, y
 def plot_data(x, y):
     """Plots y = x^2"""
     plt.figure(figsize=(8, 5))
@@ -35,10 +46,29 @@ def plot_data2(x, y):
     plt.savefig("plot.png")
     plt.show()
 
+def plot_data3(x, y):
+    """Plots y = Sin"""
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y, label="y = x³", color='purple')
+    plt.title("Plot of y = x²")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    plt.grid(True)
+    plt.legend()
+    # Save the figure
+    plt.savefig("plot.png")
+    plt.show()
+
 def main():
     print("Generating and plotting y = x²...")
     x, y = generate_data()
+    x2, y3 = generate_data2()
+    x3, y4 = generate_data2()
     plot_data(x, y)
+    plot_data2(x2, y3)
+    plot_data3(x3, y4)
+
 
 if __name__ == "__main__":
     main()
