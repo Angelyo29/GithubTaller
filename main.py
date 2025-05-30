b6233609de17d63003d40f37c96c731ef440fed2
Angelyo29 <angeloseguera29b@gmail.com>
2025-05-30 15:15:46 -0600

    Agregué la función x^3
    Signed-off-by: Angelyo29 <angeloseguera29b@gmail.com>

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ def plot_data(x, y):
     plt.savefig("plot.png")
     plt.show()
 
+def plot_data2(x, y):
+    """Plots y = x^3"""
+    plt.figure(figsize=(8, 5))
+    plt.plot(x, y, label="y = x³", color='purple')
+    plt.title("Plot of y = x²")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    plt.grid(True)
+    plt.legend()
+    # Save the figure
+    plt.savefig("plot.png")
+    plt.show()
+
 def main():
     print("Generating and plotting y = x²...")
     x, y = generate_data()
